26ed4e0dbea6a78eabc55535e9a5eafc24cee2dc
Regis100-debug <danhatawananyasha@gmail.com>
2025-05-16 17:57:56 +0200

    Initial commit

---
+++ my-first-java-spring-project/src/main/java/pl/edu/vistula/my_first_java_spring_project/MyFirstJavaSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.my_first_java_spring_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyFirstJavaSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyFirstJavaSpringProjectApplication.class, args);
+	}
+
+}
---
+++ my-first-java-spring-project/src/test/java/pl/edu/vistula/my_first_java_spring_project/MyFirstJavaSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.vistula.my_first_java_spring_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyFirstJavaSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
